8ab79ddbbad9ce4fded6b44c03df86092cc437b2
Ilya Kolebayev <kolebayev@gmail.com>
2020-01-19 23:11:50 +0300

    Improve ui, add copy to clipboard

--- src/ui/uiGetVariables.ts
+++ src/ui/uiGetVariables.ts
@@ -1,7 +1,8 @@
 import { rgb2hex } from '../utils/colorConverter'
-const btn = document.getElementById('getVars')
+const getVarsBtn = document.getElementById('getVars')
 const desk = document.getElementById('deskForVars')
-
+const copyVarsBtn = document.getElementById('copyVars')
+const textError = document.querySelector('.error_getting')
 
 const getVarsFromSelection = () => {
     parent.postMessage({
@@ -12,13 +13,25 @@ const getVarsFromSelection = () => {
     }, '*')
 }
 
-btn.addEventListener('click', getVarsFromSelection)
+getVarsBtn.addEventListener('click', getVarsFromSelection)
+
+// copyVarsBtn.addEventListener('click', () => {
+//    var el = document.createElement('textarea');
+//    el.value = desk.innerHTML;
+//    el.setAttribute('readonly', '');
+//    el.setAttribute('style','position: absolute: left: -9999px')
+//    document.body.appendChild(el);
+//    el.select();
+//    document.execCommand('copy');
+//    document.body.removeChild(el);
+// });
 
 onmessage = (e) => {
     if (e.data.pluginMessage.status === 'selectionEmpty') {
-        alert('emptySelection')
+        // alert('emptySelection')
+        textError.innerHTML = 'Select shapes on canvas'
+        setTimeout(() => textError.innerHTML = '', 1500);
     } else if (e.data.pluginMessage.status === 'selectionFilled') {
-        // console.log(e.data.pluginMessage.data)
         let varsString = ''
         e.data.pluginMessage.data.forEach(el => {
             let hex = rgb2hex(
